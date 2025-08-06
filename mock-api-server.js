6d14d9fd9ac609ddff5580a8aac776d528e9277e
sirhCC <ccharles199220@gmail.com>
2025-08-06 01:24:17 -0400

    feat: update CORS middleware to allow additional origins for development

--- mock-api-server.js
+++ mock-api-server.js
@@ -9,7 +9,7 @@ const port = 8080;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
   credentials: true
 }));
 app.use(express.json());
